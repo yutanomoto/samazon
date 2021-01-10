a98fe85f1a8ad562f8176f0f1010c47905cd4334
野元雄太 <syutkou.gja@gmail.com>
2021-01-10 12:13:34 +0000

    userinfomation finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions',
@@ -14,6 +15,15 @@ Rails.application.routes.draw do
     get "login", :to => "users/sessions#new"
     get "logout", :to => "users/sessions#destroy"
   end
+  
+  resources :users, only: [:edit, :update] do
+    collection do
+      get "mypage", :to => "users#mypage"
+      get "mypage/edit", :to => "users#edit"
+      get "mypage/address/edit", :to => "users#edit_address"
+      put "mypage", :to => "users#update"
+   end
+ end
     resources :products do
      member do
      get :favorite
