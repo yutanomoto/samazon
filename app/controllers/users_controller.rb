98864551b843faa70a48d89f4192098aae1125e9
野元雄太 <syutkou.gja@gmail.com>
2021-01-10 12:44:10 +0000

    passward finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user
+  # ログインしていないユーザのアクセスを制限する。
+  before_action :authenticate_user!
 
   def edit
    
@@ -19,6 +21,20 @@ class UsersController < ApplicationController
   
   end
   
+ def update_password
+   if password_set?
+     @user.update_password(user_params) 
+     flash[:notice] = "パスワードは正しく更新されました。"
+     redirect_to root_url
+   else
+     @user.errors.add(:password, "パスワードに不備があります。")
+     render "edit_password"
+   end
+ end
+ 
+  def edit_password
+  end
+  
   private
   
      def set_user
@@ -26,5 +42,10 @@ class UsersController < ApplicationController
      end
   def user_params
      params.permit(:name, :email, :address, :phone, :password, :password_confirmation)
+  end
+  
+  def password_set?
+      user_params[:password].present? && user_params[:password_confirmation].present? ?
+      true : false
   end
 end
